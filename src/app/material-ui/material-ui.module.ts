b68d85724abf812e5e8b0eabdd89f96f3ae0e15d
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-19 17:44:59 +0100

    Aggiunti badges
    Aggiunti esempi per i badges

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
 
 const UserMaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
-  MatIconModule
+  MatIconModule,
+  MatBadgeModule
 ];
 
 @NgModule({
