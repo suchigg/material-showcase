700622817e874b8b759e0d6f1adf63ae01c112f1
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-19 10:11:25 +0100

    Toggle buttons
    Aggiunti esempi di selezione singola e multipla

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 const UserMaterialComponents = [
   MatButtonModule,
+  MatButtonToggleModule,
   MatIconModule
 ];
 
