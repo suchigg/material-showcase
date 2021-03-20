9404792d2423a4a70d78b55a6b0476e6bf26834e
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-20 16:51:54 +0100

    Grid list
    Esempi di griglia base, occupazione delle celle, altezza e gutter

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -10,6 +10,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 const UserMaterialComponents = [
   MatButtonModule,
@@ -22,7 +23,8 @@ const UserMaterialComponents = [
   MatSidenavModule,
   MatMenuModule,
   MatListModule,
-  MatDividerModule
+  MatDividerModule,
+  MatGridListModule
 ];
 
 @NgModule({
