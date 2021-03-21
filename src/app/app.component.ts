2444ef55bc136c155f995fafa9f8f0c26b2a3853
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-21 18:00:11 +0100

    Table Sorting
    Aggiunto un esempio di tabella ordinabile tramite mat sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,99 +1,6 @@
-import { DialogExampleComponent } from './dialog-example/dialog-example.component';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent implements OnInit {
-  title = 'components-showcase';
-
-  opened = false;
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  selectedValue: string;
-  selectedFEValue: string;
-
-  options = ['angular', 'vue', 'react', 'ember', 'jquery'];
-  filteredOptions: Observable<string[]>;
-  optionsObj = [{name: 'Angular', value: 'angular'}, {name: 'React', value: 'react'}, {name: 'Vue', value: 'vue'}]
-  displayFn = (item) => {return item.value || undefined}
-
-  tech = new FormControl('', [Validators.required]);
-
-  ngOnInit() {
-    this.filteredOptions = this.tech.valueChanges
-      .pipe(
-        startWith(''),
-        map(value => this._filter(value))
-      );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-  minDate = new Date();
-  maxDate = new Date(2021, 2, 30);
-
-  excludeSaturdayAndSunday = (d: Date | null): boolean => {
-    const day = (d || new Date()).getDay();
-    // Prevent Saturday and Sunday from being selected.
-    return day !== 0 && day !== 6;
-  }
-
-  constructor(private readonly snackBar: MatSnackBar,
-              private readonly dialog: MatDialog) {}
-
-  public openSnackbar(msg: string, action?: string): void {
-    const conf: MatSnackBarConfig = {duration: 2000};
-    const snackbarRef = this.snackBar.open(msg, action, conf);
-
-    snackbarRef.afterDismissed().subscribe( () => console.log('snackbar dismissed'));
-    snackbarRef.onAction().subscribe( () => console.log('the snackbar action was triggerd'));
-  }
-
-  public openCustomSnackbar(msg: string): void {
-    this.snackBar.openFromComponent(CustomSnackbarComponent, {duration: 2000})
-  }
-
-  public openDialog(): void {
-    const dialogRef = this.dialog.open(DialogExampleComponent, {data: {name: 'Luca'}});
-
-    dialogRef.afterClosed().subscribe(res => {
-      console.log(`Dialog result: ${res}`);
-    });
-  }
-
-  /** table */
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-
-  filteredDataSource = new MatTableDataSource(ELEMENT_DATA);
-
-  public applyFilter(value: string = ''): void {
-    this.filteredDataSource.filter = value.trim().toLowerCase();
-  }
-
-}
 
 export interface PeriodicElement {
   name: string;
@@ -115,15 +22,24 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
+
 @Component({
-  selector: 'custom-snackbar',
-  template: `<span>My custom snackbar</span>`,
-  styles: [
-    `span {
-      color: orange;
-    }`
-  ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class CustomSnackbarComponent {}
+export class AppComponent implements AfterViewInit {
+  title = 'components-showcase';
 
+  /** table */
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  sortedDataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit(): void {
+    /** table */
+    this.sortedDataSource.sort = this.sort;
+  }
+}
