cc04fbebeb33540eabe530a5b7cd45bd90f1b159
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-21 15:37:08 +0100

    Tooltip
    Aggiunti esempi di tooltip semplice, con apertura e chiusura ritardata e con posizione alterata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,55 +8,8 @@ import { map, startWith } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'components-showcase';
-
-  opened = false;
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  selectedValue: string;
-  selectedFEValue: string;
-
-  options = ['angular', 'vue', 'react', 'ember', 'jquery'];
-  filteredOptions: Observable<string[]>;
-  optionsObj = [{name: 'Angular', value: 'angular'}, {name: 'React', value: 'react'}, {name: 'Vue', value: 'vue'}]
-  displayFn = (item) => {return item.value || undefined}
-
-  tech = new FormControl('', [Validators.required]);
-
-  ngOnInit() {
-    this.filteredOptions = this.tech.valueChanges
-      .pipe(
-        startWith(''),
-        map(value => this._filter(value))
-      );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-  minDate = new Date();
-  maxDate = new Date(2021, 2, 30);
-
-  excludeSaturdayAndSunday = (d: Date | null): boolean => {
-    const day = (d || new Date()).getDay();
-    // Prevent Saturday and Sunday from being selected.
-    return day !== 0 && day !== 6;
-  }
-
-
 }
 
 
