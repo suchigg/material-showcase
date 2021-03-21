a2baee6195b7636c3ca58763c67c55f84d7852c7
Luca Farsetti <luca.farsetti@lascaux.it>
2021-03-21 12:50:45 +0100

    Autocomplete
    Aggiunto un esempio di autocomplete e di opzione filtrata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'components-showcase';
 
   opened = false;
@@ -24,4 +26,28 @@ export class AppComponent {
   selectedValue: string;
   selectedFEValue: string;
 
+  options = ['angular', 'vue', 'react', 'ember', 'jquery'];
+  filteredOptions: Observable<string[]>;
+  optionsObj = [{name: 'Angular', value: 'angular'}, {name: 'React', value: 'react'}, {name: 'Vue', value: 'vue'}]
+  displayFn = (item) => {return item.value || undefined}
+
+  tech = new FormControl('', [Validators.required]);
+
+  ngOnInit() {
+    this.filteredOptions = this.tech.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+
 }
+
+
